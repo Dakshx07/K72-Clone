75e7d3cab48d7af40716b53dc67ff88846fcdee0
Dakshx07 <daksh.hiran@gmail.com>
2025-12-29 16:14:29 +0530

    add liveClock, privacy policies,buttons

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -5,13 +5,13 @@ import { NavBarContext } from '../../../context/NavContext'
 import LiveClock from '../home/LiveClock'
 
 const FullScreenNav = () => {
-    
-    const fullNavLinkRef=useRef(null)
-    const fullScreenRef=useRef(null)
-    const [navOpen,setNavOpen]=useContext(NavBarContext)
+
+    const fullNavLinkRef = useRef(null)
+    const fullScreenRef = useRef(null)
+    const [navOpen, setNavOpen] = useContext(NavBarContext)
     console.log(navOpen);
-    
-     
+
+
     function gsapAnimation() {
         const tl = gsap.timeline()
         tl.to('.fullscreennav', {
@@ -36,8 +36,8 @@ const FullScreenNav = () => {
         })
     }
 
-        
-      function gsapAnimationReverse() {
+
+    function gsapAnimationReverse() {
         const tl = gsap.timeline()
         tl.to('.link', {
             opacity: 0,
@@ -70,11 +70,11 @@ const FullScreenNav = () => {
 
         }
     }, [navOpen])
-    
-    
+
+
     return (
         <div ref={fullScreenRef} className='fullscreennav hidden h-screen  overflow-x-hidden w-full z-50  absolute'>
-            <div  className='h-screen w-full fixed'>
+            <div className='h-screen w-full fixed'>
                 <div className='h-full flex w-full '>
                     <div className='stairing h-full w-1/5 bg-black'></div>
                     <div className='stairing h-full w-1/5 bg-black'></div>
@@ -95,8 +95,8 @@ const FullScreenNav = () => {
                     <div onClick={() => {
                         setNavOpen(false)
                     }} className='h-32 w-32 relative cursor-pointer'>
-                        <div className='h-40 w-1 absolute -rotate-48 origin-top bg-[#D3FD50]'></div>
-                        <div className='h-40 w-1 absolute right-0 rotate-48 origin-top bg-[#D3FD50]'></div>
+                        <div className='h-40 w-1 absolute -rotate-48 origin-top bg-white'></div>
+                        <div className='h-40 w-1 absolute right-0 rotate-48 origin-top bg-white'></div>
 
                     </div>
                 </div>
@@ -176,10 +176,47 @@ const FullScreenNav = () => {
 
                 </div>
 
-                <div className='flex pt-4 text-2xl font-[font1]'>
-                    <h6 >MONTREAL_</h6>
-                    <LiveClock />
+
+                <div className='flex justify-between items-centre'>
+                    
+                    <div className='bottom-0 left-0 flex gap-2'>
+                    <div className='flex  text-2xl font-[font1]'>
+                        <div className='pt-13 pr-3' >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                            </svg>
+                        </div>
+                        <div className='flex pt-15 text-2xl font-[font1]'>
+                            <h6 >MONTREAL_</h6>
+                        <LiveClock />
+                        </div>
+                    </div>
+                    </div>
+
+                    
+              
+                    <div className='text-[0.85rem] gap-2 pt-15 flex items-center justify-center font-[font1]'>
+                        <h4 className='hover:text-[#D3FD50] cursor-pointer'>Privacy Policy</h4>
+                        <h4 className='hover:text-[#D3FD50] cursor-pointer'>Privacy Notice</h4>
+                        <h4 className='hover:text-[#D3FD50] cursor-pointer'>Ethics Report</h4>
+                        <h4 className='hover:text-[#D3FD50] cursor-pointer'>Constent Choices</h4>
+                    </div>
+            
+
+                
+
+                
+                
+                    <div className='flex items-center gap-2 font-[font2] text-[2vw]  pt-4'>
+                        <h1 className='border-2 rounded-[999rem] overflow-hidden p-4 hover:text-[#D3FD50] cursor-pointer'>FB</h1>
+                        <h1 className='border-2 rounded-[999rem] overflow-hidden p-4 hover:text-[#D3FD50] cursor-pointer'>IG</h1>
+                        <h1 className='border-2 rounded-[999rem] overflow-hidden p-4 hover:text-[#D3FD50] cursor-pointer'>IN</h1>
+                        <h1 className='border-2 rounded-[999rem] overflow-hidden p-4 hover:text-[#D3FD50] cursor-pointer'>BE</h1>
+                    </div>
+              
+
                 </div>
+
             </div>
         </div>
     )
